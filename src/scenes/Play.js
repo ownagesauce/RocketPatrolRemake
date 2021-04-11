8e8dc3d5ebacb9a3aa71b35a3b8d6eccc66593a5
ownagesauce <ejtung@ucsc.edu>
2021-04-10 19:10:29 -0700

    added firing functionality

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,6 +9,7 @@ class Play extends Phaser.Scene {
     preload()  {
 
        this.load.image('starfield', 'assets/starfield.png');
+       this.load.image('rocket', 'assets/rocket.png');
 
     }
 
@@ -18,6 +19,8 @@ class Play extends Phaser.Scene {
         this.starfield = this.add.tileSprite(
             0, 0, 640, 480, 'starfield'
         ).setOrigin(0, 0);
+
+        this.p1Rocket = new Rocket(this, game.config.width / 2, game.config.height - borderUISize - borderPadding, 'rocket');
 
 
         // green UI background
@@ -29,11 +32,17 @@ class Play extends Phaser.Scene {
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
     }
 
     update() {
 
         this.starfield.tilePositionX -= 2;
+        this.p1Rocket.update();
 
     }
 

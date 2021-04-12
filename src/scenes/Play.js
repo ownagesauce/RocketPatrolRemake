2c0c87d2414a434e9e459d4c4bf446c87376242d
ownagesauce <ejtung@ucsc.edu>
2021-04-11 18:23:17 -0700

    github fix attempt 1

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,10 +8,10 @@ class Play extends Phaser.Scene {
 
     preload()  {
 
-       this.load.image('starfield', 'assets/starfield.png');
-       this.load.image('rocket', 'assets/rocket.png');
-       this.load.image('spaceship', 'assets/spaceship.png');
-       this.load.spritesheet('explosion', 'assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+       this.load.image('starfield', './assets/starfield.png');
+       this.load.image('rocket', './assets/rocket.png');
+       this.load.image('spaceship', './assets/spaceship.png');
+       this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
 
     }
 
@@ -90,7 +90,7 @@ class Play extends Phaser.Scene {
             this.scene.restart();
         }
 
-        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
             this.scene.start("menuScene");
         }
 
